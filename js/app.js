61efc86b219704c506d938c2dda66bd31e9e8cd6
Pedro Rachewsky <pedrorachewsky@hotmail.com>
2015-08-04 13:28:49 -0300

    refactor code, add collision, fix enemy loop and creation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 // Enemies our player must avoid
+
+
+// Random enemy speed
+var enemyRandomSpeed = function() {
+    return 120 + Math.floor(Math.random() * 150);
+}
+// Random enemy X starting position
+var enemyXrandom = function() {
+    return -100 - Math.floor(Math.random(0.7, 1)*100);
+}
+
+// Enemy class
 var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -8,7 +20,7 @@ var Enemy = function(x,y) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
-    this.speed = 50 * (Math.floor(Math.random() * 2)+1);
+    this.speed = enemyRandomSpeed();
 }
 
 // Update the enemy's position, required method for game
@@ -18,7 +30,22 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed * dt;
-    //this.y *= dt;
+
+     // Enemy loop (if enemy goes out of bounds, reset random x and speed)
+    if(this.x > 700) {
+        this.x = enemyXrandom();
+        this.speed = enemyRandomSpeed();
+    };
+
+    // More enemies (between 3 and 6)
+    if(this.x > 300 && allEnemies.length > 2 && allEnemies.length < 6) {
+        createEnemy()
+    };
+
+    // Colision detection - player vs enemies
+    if((this.y > player.y - 20 && this.y < player.y + 20) && (this.x > player.x - 70 && this.x < player.x + 70)) {
+        player.reset();
+    }
 }
 
 // Draw the enemy on the screen, required method for game
@@ -30,15 +57,16 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+// Player class
 var Player = function(x,y) {
     this.sprite = 'images/char-boy.png';
     this.x = x;
     this.y = y;
 }
 
-Player.prototype.update = function(dt) {
-    //this.x *=  dt;
-    //this.y *=  dt;
+Player.prototype.update = function() {
+    this.x =  this.x;
+    this.y =  this.y;
 }
 
 Player.prototype.render = function() {
@@ -46,7 +74,6 @@ Player.prototype.render = function() {
 }
 
 Player.prototype.handleInput = function(key) {
-    //console.log(key);
     switch(key) {
         case 'left':
             this.x -= 100;
@@ -61,15 +88,16 @@ Player.prototype.handleInput = function(key) {
             this.y += 81;
             break;
     }
+
+    // Player game bounds
     if (this.x < 0) {
         this.x = 0;
     } else if (this.x > 401) {
         this.x = 400;
     } else if (this.y > 401) {
         this.y = 400;
-    } else if (this.y < -6) {
-        this.x = 200;
-        this.y = 400;
+    } else if (this.y <= -20) {
+        player.reset();
     }
 }
 
@@ -77,31 +105,30 @@ Player.prototype.handleInput = function(key) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-// Random X starting position for enemy
-var xRandom = function() {
-    return Math.random(0.8, 1)*100-400
-}
-
-// Enemy starting grid position
-var enemyRows = [[xRandom(), 60],[xRandom(), 145],[xRandom(), 230]];
-
+// Enemies array
 var allEnemies = [];
+
+// Enemy starting grid position - 3 possible rows - random X
+var enemyRows = [[enemyXrandom(), 60],[enemyXrandom(), 145],[enemyXrandom(), 230]];
 
 // Enemy creation per row at starting random position
 
-setInterval(function() {
+function createEnemy(row) {
     for(var i = 0; i < 3; i++) {
-        var enemy = new Enemy(enemyRows[i][0], enemyRows[i][1]);
-        allEnemies.push(enemy);
-        if (allEnemies.length > 25) {
-        allEnemies.shift();
-        }
+            var enemy = new Enemy(enemyRows[i][0], enemyRows[i][1]);
+            allEnemies.push(enemy);
     }
-}, 5000);
+}
+createEnemy();
 
+// Player creation at fixed starting position
+var player = new Player(200,390);
 
-// Player creation at starting position
-var player = new Player(200,400);
+// Player resets to starting position if collision or win
+Player.prototype.reset = function() {
+            this.x = 200;
+            this.y = 390;
+        }
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
